1282a0194dac9438e0df3188cb186b5c88198435
JGoldsmith6321 <JGoldsmith6321@gmail.com>
2015-09-16 21:34:42 -0400

    part 2

---
+++ Project 1/Assets/CubeAppear.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeAppear : MonoBehaviour {
+	public float waitTime;
+	public GameObject cube;
+	public bool hasCube;
+	public float timeWaiting;
+
+	// Use this for initialization
+	void Start () {
+		timeWaiting = 1F;
+		waitTime = Time.time + timeWaiting;
+		hasCube = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= waitTime && hasCube==false) {
+			cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			hasCube = true;
+		}
+	}
+}
---
+++ Project 1/Assets/PausingOnStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PausingOnStart : MonoBehaviour {
+	public float timeToSwitch;
+	// Use this for initialization
+	void Start () {
+		timeToSwitch = Time.time + 0.05F;
+	}
+
+	void OnGUI (){
+		if (Time.time >= timeToSwitch) {
+			Application.LoadLevel("2");
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
